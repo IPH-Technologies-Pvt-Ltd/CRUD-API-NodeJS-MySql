0889536cfcc81036d975ff0835052b5ba6db9546
Saurabh yadav <saurabh.yadav@iphtechnologies.com>
2023-05-22 16:36:10 +0530

    crud api

---
+++ crud-api/api/users/massage.js
@@ -0,0 +1,9 @@
+module.exports = {
+    databaseConnectionError: 'Database not connected',
+    invalidEmailOrPassword: 'Invalid email or password',
+    loginSuccess: 'Login successful',
+    recordNotFound: 'Record not found',
+    updatedSuccessfully: 'Updated successfully',
+    userDeletedSuccessfully: 'User deleted successfully'
+  };
+  
